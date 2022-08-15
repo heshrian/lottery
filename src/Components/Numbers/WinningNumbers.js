18910a090bba04eacba59d77862004aa11154c43
heshrian <kamburovviktor@gmail.com>
2022-08-16 00:27:50 +0100

    console.log double trouble

--- src/Components/Numbers/WinningNumbers.js
+++ src/Components/Numbers/WinningNumbers.js
@@ -1,7 +1,7 @@
 const WinningNumbers = (props) => {
   let numbers = [];
 
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 5; i++) {
     let num = Math.round(Math.random() * 90);
     if (numbers.includes(num)) {
       num = Math.random() * 90;
@@ -10,7 +10,8 @@ const WinningNumbers = (props) => {
     }
   }
 
-  return numbers.sort((a, b) => a - b);
+  return props.getNumbers(numbers.sort((a, b) => a - b));
+//   return props.getNumbers([1,2,5,7]);
 };
 
 export default WinningNumbers;
--- src/Components/Pages/IndexPage.js
+++ src/Components/Pages/IndexPage.js
@@ -3,14 +3,19 @@ import React, { useState } from "react";
 import WinningNumbers from "../Numbers/WinningNumbers";
 
 const IndexPage = (props) => {
-  const [isShown, setIsShown] = useState(true);
+  const [isShown, setIsShown] = useState(false);
   const handleClick = (event) => {
     setIsShown((current) => !current);
   };
 
+  const getNumbers = (array) => {
+    // console.log(array.map(e => e))
+  }
+
   const initialPage = (
     <div className="starting_">
       <button className="button_" type="submit" onClick={handleClick}>
+        <WinningNumbers getNumbers={getNumbers} />
         Click for the winning Lottery Numbers!
       </button>
     </div>
